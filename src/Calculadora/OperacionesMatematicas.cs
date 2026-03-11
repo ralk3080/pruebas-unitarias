76169b8fd2cf236d4addc3aaa5deca2ee9426e43
ralk3080 <ralk3080@gmail.com>
2026-03-11 14:50:39 -0600

    se añade formar (#5)

--- src/Calculadora/OperacionesMatematicas.cs
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -7,21 +7,21 @@ public class OperacionesMatematicas
         return a + b;
     }
 
-    public double Restar(double a, double b)
+                    public double Restar(double a, double b)
     {
         return a - b;
     }
 
     public double Multiplicar(double a, double b)
-    {
+                        {
         return a * b;
     }
 
-    public double Dividir(double a, double b)
+                        public double Dividir(double a, double b)
     {
         if (b == 0)
         {
-            throw new DivideByZeroException("No se puede dividir por cero.");
+                            throw new DivideByZeroException("No se puede dividir por cero.");
         }
         return a / b;
     }
