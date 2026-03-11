0f85872d833c78720bfc8d0757677b816bbee2d2
Raul Diaz <ralk3080@gmail.com>
2026-03-11 14:37:52 -0600

    se agregan pruebas unitarias de resa

--- tests/Calculadora.tests/OperacionesMatematicasTests.cs
+++ tests/Calculadora.tests/OperacionesMatematicasTests.cs
@@ -49,5 +49,47 @@ public class OperacionesMatematicasTests
       // Assert
       Assert.Equal(0, resultado);
     }
+    
+    [Fact]
+public void Resta_DosPositivos_RetornaLaResta()
+{
+    // Arrange
+    int a = 5;
+    int b = 3;
+
+    // Act
+    var resultado = _operacionesMatematicas.Restar(a, b);
+
+    // Assert
+    Assert.Equal(2, resultado);
+}
+
+    [Fact]
+    public void Resta_NegativoMenosPositivo_RetornaValorCorrecto()
+    {
+        // Arrange
+        int a = -5;
+        int b = 3;
+
+        // Act
+        var resultado = _operacionesMatematicas.Restar(a, b);
+
+        // Assert
+        Assert.Equal(-8, resultado);
+    }
+
+    [Fact]
+    public void Resta_CeroMenosCero_RetornaCero()
+    {
+        // Arrange
+        int a = 0;
+        int b = 0;
+
+        // Act
+        var resultado = _operacionesMatematicas.Restar(a, b);
+
+        // Assert
+        Assert.Equal(0, resultado);
+    }
 
 }
