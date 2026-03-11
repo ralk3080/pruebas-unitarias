e0c808a267034a44a7998e7c02c743995c00e3f5
ralk3080 <ralk3080@gmail.com>
2026-03-11 13:25:00 -0600

    Se sube archivo yml (#1)

--- src/Calculadora/OperacionesMatematicas.cs
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -1,4 +1,4 @@
-namespace Calaculadora;
+namespace Calculadora;
 
 public class OperacionesMatematicas
 {
--- tests/Calculadora.tests/OperacionesMatematicasTests.cs
+++ tests/Calculadora.tests/OperacionesMatematicasTests.cs
@@ -1,8 +1,53 @@
-using Calculadora
+using Calculadora;
 
 namespace Calculadora.Tests;
 
 public class OperacionesMatematicasTests
 {
-    
+
+    //Arrange
+    private readonly OperacionesMatematicas _operacionesMatematicas = new();
+
+    [Fact]
+    public void Sumar_DosPositivos_RetornaLaSuma()
+    {
+      // Arrange
+      int a = 5; 
+      int b = 3;
+
+      // Act
+      var resultado = _operacionesMatematicas.Sumar(a, b);
+
+      // Assert
+      Assert.Equal(8, resultado);
+    }
+
+    [Fact]
+    public void Sumar_NegativoMasPositivo_RetornaValorCorrecto()
+    {
+      // Arrange
+      int a = -5; 
+      int b = 3;
+
+      // Act
+      var resultado = _operacionesMatematicas.Sumar(a, b);
+
+      // Assert
+      Assert.Equal(-2, resultado);
+    }
+
+    [Fact]
+    public void Sumar_CoreoMasCero_RetornaCero()
+    {
+      // Arrange
+      int a = 0; 
+      int b = 0;
+
+      // Act
+      var resultado = _operacionesMatematicas.Sumar(a, b);
+
+      // Assert
+      Assert.Equal(0, resultado);
+    }
+
 }
