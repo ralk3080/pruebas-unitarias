83d0a700795a671417018bc69438c11dfdcd1ef2
Raul Diaz <ralk3080@gmail.com>
2026-03-11 12:08:46 -0600

    se suben avances 2

---
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -0,0 +1,33 @@
+namespace Calaculadora;
+
+public class OperacionesMatematicas
+{
+    public double Sumar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Restar(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Dividir(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("No se puede dividir por cero.");
+        }
+        return a / b;
+    }
+
+    public bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
